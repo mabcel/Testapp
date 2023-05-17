9a84a7e5e812e99f8be95bb03205c56be68053fc
ARABA <mabcelle@gmail.com>
2023-05-17 20:24:52 +0000

    phase 3

---
+++ Java Phase 3/SpringBootThymeleafAndSpringData/SpringBootThymeleafAndSpringData/src/main/java/com/entity/Orders.java
@@ -0,0 +1,40 @@
+package com.entity;
+
+public class Orders {
+private int oid;
+private String productname;
+private float price;
+private int qty;
+private String customername;
+public int getOid() {
+	return oid;
+}
+public void setOid(int oid) {
+	this.oid = oid;
+}
+public String getProductname() {
+	return productname;
+}
+public void setProductname(String productname) {
+	this.productname = productname;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+public int getQty() {
+	return qty;
+}
+public void setQty(int qty) {
+	this.qty = qty;
+}
+public String getCustomername() {
+	return customername;
+}
+public void setCustomername(String customername) {
+	this.customername = customername;
+}
+
+}
